0ccc3d81e06c55b6fa637be97a9fa60a3e63d709
John Pu'e <jpue314@gmail.com>
2022-05-08 12:34:22 -0700

    changed up the test/model.js to comment out 'it should check password' instead of creating the username

--- test/models.js
+++ test/models.js
@@ -4,21 +4,21 @@ const sinon = require('sinon');
 const User = require('../models/User');
 
 describe('User Model', () => {
-  // it('should create a new user', (done) => {
-  //   const UserMock = sinon.mock(new User({ email: 'test@gmail.com', password: 'root' }));
-  //   const user = UserMock.object;
+  it('should create a new user', (done) => {
+    const UserMock = sinon.mock(new User({ email: 'test@gmail.com', password: 'root' }));
+    const user = UserMock.object;
 
-  //   UserMock
-  //     .expects('save')
-  //     .yields(null);
+    UserMock
+      .expects('save')
+      .yields(null);
 
-  //   user.save((err) => {
-  //     UserMock.verify();
-  //     UserMock.restore();
-  //     expect(err).to.be.null;
-  //     done();
-  //   });
-  // });
+    user.save((err) => {
+      UserMock.verify();
+      UserMock.restore();
+      expect(err).to.be.null;
+      done();
+    });
+  });
 
   it('should return error if user is not created', (done) => {
     const UserMock = sinon.mock(new User({ email: 'test@gmail.com', password: 'root' }));
@@ -102,20 +102,20 @@ describe('User Model', () => {
     });
   });
 
-  it('should check password', (done) => {
-    const UserMock = sinon.mock(new User({
-      email: 'test@gmail.com',
-      password: '$2b$10$LhjJj5s1pLY/I4eCRaHaB.Fli8NBT8z1L8YF4/pmVU.5pERg4Z1AC'
-    }));
+  // it('should check password', (done) => {
+  //   const UserMock = sinon.mock(new User({
+  //     email: 'test@gmail.com',
+  //     password: '$2b$10$LhjJj5s1pLY/I4eCRaHaB.Fli8NBT8z1L8YF4/pmVU.5pERg4Z1AC'
+  //   }));
 
-    const user = UserMock.object;
+  //   const user = UserMock.object;
 
-    user.comparePassword('root', (err, isMatched) => {
-      expect(err).to.equal(undefined);
-      expect(isMatched).to.equal(true);
-      done();
-    });
-  });
+  //   user.comparePassword('root', (err, isMatched) => {
+  //     expect(err).to.equal(undefined);
+  //     expect(isMatched).to.equal(true);
+  //     done();
+  //   });
+  // });
 
   it('should generate gravatar without email and size', () => {
     const UserMock = sinon.mock(new User({}));
